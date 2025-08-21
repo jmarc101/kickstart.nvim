b548fad3404a438ddef48930242238a49c8ba61c
Jean-Marc Prud'homme <42589969+jmarc101@users.noreply.github.com>
2025-08-21 12:57:10 -0400

    latest

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -133,3 +133,6 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Misc.
+vim.keymap.set('n', '<leader>fp', ":let @+ = expand('%')<CR>", { desc = 'Copy file path to cliboard' })
